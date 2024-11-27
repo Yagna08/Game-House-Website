1d0fe762d76a4f4bebe6e3d9f2c84132a0ee31c0
Yagna08 <yagnapatel2108@gmail.com>
2024-11-27 21:35:13 +0530

    deployment need

--- Backend/server.js
+++ Backend/server.js
@@ -69,7 +69,7 @@ app.post('/api/user/signup', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-// module.exports = app;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+module.exports = app;
